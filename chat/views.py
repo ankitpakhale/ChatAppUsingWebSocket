239eaa85c6a61006367f92c37da263bdef229445
ankitpakhale <akp3067@gmail.com>
2022-03-16 16:06:40 +0530

    working

--- chat/views.py
+++ chat/views.py
@@ -5,12 +5,9 @@ from .models import Message
 def index(request):
   return render(request, 'chat/index.html')
 
-
 def room(request, room_name):
   username = request.GET.get('username', 'Anonymous')
-
   messages = Message.objects.filter(room=room_name)[0:25]
-  
   return render(request, 'chat/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
   
   
