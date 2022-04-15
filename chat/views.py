3990974f536ff5606a1f411fea19d2021bb15e70
ankitpakhale <akp3067@gmail.com>
2022-04-15 23:20:47 +0530

    Row commit

--- chat/views.py
+++ chat/views.py
@@ -1,20 +1,12 @@
-import re
 from django.shortcuts import render
+
 from .models import Message
-# Create your views here.
 
 def index(request):
-  return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html')
 
 def room(request, room_name):
-  username = request.GET.get('username', 'Anonymous')
-  messages = Message.objects.filter(room=room_name)[0:25]
-  return render(request, 'chat/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
-  
-def demo(request):
-  return render(request, 'chat/demo.html')  
+    username = request.GET.get('username', 'Anonymous')
+    messages = Message.objects.filter(room=room_name)[0:25]
 
-
-
-
-  
+    return render(request, 'chat/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
--- chatty/asgi.py
+++ chatty/asgi.py
@@ -1,27 +1,8 @@
-"""
-ASGI config for chatty project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
-# -------------------------------------old code-------------------------------------
-# import os
-
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatty.settings')
-
-# application = get_asgi_application()
-
-# -------------------------------------new code-------------------------------------
-
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
 
 import chat.routing
@@ -29,10 +10,10 @@ import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatty.settings')
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-    URLRouter(
-      chat.routing.websocket_urlpatterns
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     )
-  )
 })
