b7c53b6d85cd2ac33a4f64f1463c01bc62ed0952
ankitpakhale <akp3067@gmail.com>
2022-04-18 09:05:44 +0530

    Raw Code without any changes

--- chat/views.py
+++ chat/views.py
@@ -8,4 +8,5 @@ def index(request):
 def room(request, room_name):
     username = request.GET.get('username', 'Anonymous')
     messages = Message.objects.filter(room=room_name)[0:25]
+
     return render(request, 'chat/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
