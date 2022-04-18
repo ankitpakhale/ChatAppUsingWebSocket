fd4b6ba0faeec282804c4deffb9718f5d6f2df0c
ankitpakhale <akp3067@gmail.com>
2022-04-18 23:57:43 +0530

    Updated: Requirements.txt

--- chatty/chatty/settings.py
+++ chatty/chatty/settings.py
@@ -76,11 +76,10 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        }
-    }
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
